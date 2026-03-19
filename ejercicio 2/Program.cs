6c82ac42537577b89d3c64efb738bdbcc6c9a45a
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-19 09:23:15 -0600

    ejercicio 2 terminado

---
+++ ejercicio 2/Program.cs
@@ -0,0 +1,24 @@
+﻿double precio;
+bool correcto;
+
+do
+{
+    Console.Clear();
+        Console.WriteLine("Ingrese el precio del producto: ");
+    bool esNumero= double.TryParse(Console.ReadLine(), out precio);
+
+    if (precio > 0 && esNumero)
+    {
+        correcto = true;
+    }
+    else
+    {
+        correcto = false;   
+        Console.WriteLine("Precio inválido o fuera de rango.");
+        Console.WriteLine("Presione cualquier tecla para intentar otra vez...");
+    Console.ReadKey();
+    }
+}while (!correcto);
+Console.Clear();
+Console.WriteLine("Precio válido: Q" + precio);
+Console.ReadKey();
