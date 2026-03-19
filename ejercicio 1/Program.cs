d2cb22d6208a755068b8151d8f3d2c468d8c3327
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-19 09:01:03 -0600

    ejercicio 1 terminado

--- ejercicio 1/Program.cs
+++ ejercicio 1/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿int edad;
+bool correcto;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese su edad: ");
+    bool esNumero = int.TryParse(Console.ReadLine(), out edad);
+
+    if (esNumero && edad >= 0 && edad <= 110)
+    {
+        correcto = true;
+    }
+    else
+    {
+        correcto = false;
+        Console.WriteLine("Dato inválido o fuera de rango (0-110).");
+        Console.WriteLine("Presione cualquier tecla para intentar otra vez...");
+        Console.ReadKey();
+    }
+
+} while (!correcto);
+
+Console.Clear();
+Console.WriteLine("Edad válida: " + edad);
+Console.ReadKey();
