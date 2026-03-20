492d4ba52c17b204dfda0092374f09fc3b10bb51
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-03-19 19:35:53 -0600

    ejercicio 3 finalizado

---
+++ ejercicio 3/Program.cs
@@ -0,0 +1,25 @@
+﻿int estudiantes;
+bool correcto;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese el número de estudiantes: ");
+    bool esNumero = int.TryParse(Console.ReadLine(), out estudiantes);
+
+    if (esNumero && estudiantes > 0)
+    {
+        correcto = true;
+    }
+    else
+    {
+        correcto = false;
+        Console.WriteLine("Número inválido o Contiene letras.");
+        Console.WriteLine("Presione cualquier tecla para intentar otra vez...");
+        Console.ReadKey();
+    }
+} while (!correcto) ;
+Console.Clear();
+Console.WriteLine("Cantidad de estudiantes correcta");
+Console.WriteLine("Pesione cualquier tecla para salir");
+Console.ReadKey();
